38cdcba62684448276e2b8dae21fc8f900450f77
Felix Manke <felix.manke@ext.elli.eco>
2021-10-17 20:06:30 +0200

    Add DaisyUI and use it in Button component.

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -3,19 +3,19 @@
 </script>
 
 <button on:click class="
+  btn btn-primary
   w-full
   my-4
-  px-8 py-4
   text-indigo-600
   bg-purple-100
-  active:bg-purple-200
+  hover:bg-purple-200
+  active:bg-purple-300
   rounded-full
   outline-none
   focus:ring-2
   focus:ring-indigo-600
   active:ring-offset-2
   active:ring-4
-  "
->
+  ">
   <slot>Custom Button</slot>
 </button>
